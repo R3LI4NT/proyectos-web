0a1260d385bc3b775076a59ea7671730e3cff09e
R3LI4NT <75953873+R3LI4NT@users.noreply.github.com>
2022-07-05 16:40:48 -0300

    Delete Menus Navegación/9- Menú Responsive v4 directory

--- Menus Navegación/9- Menú Responsive v4/script.js
+++
@@ -1,6 +0,0 @@
-var hamburguer = document.getElementById("Hamburguer")
-var menu = document.getElementById("Menu")
-
-hamburguer.addEventListener("click", function(){
-    menu.classList.toggle('open')
-});
