e97a72171eabf70917836b228d6815598dd3fc30
R3LI4NT <75953873+R3LI4NT@users.noreply.github.com>
2022-07-04 22:17:34 -0300

    Delete 1- Menú Lateral directory

--- 1- Menú Lateral/scripts.js
+++
@@ -1,6 +0,0 @@
-const $abrirOcultar = document.getElementById("abrir-ocultar"),
-      $menu = document.getElementById("menu");
-
-$abrirOcultar.addEventListener("click",()=>{
-    $menu.classList.toggle("desplegar")
-})      
