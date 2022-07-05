08afa4176c8afe9982294e08ab3aaa7c87061b04
R3LI4NT <75953873+R3LI4NT@users.noreply.github.com>
2022-07-04 22:18:30 -0300

    Delete 7- Menú Responsive v3 directory

--- 7- Menú Responsive v3/app.js
+++
@@ -1,6 +0,0 @@
-function openClose(){
-    let menu = document.querySelector('.menu');
-    let interactuar = document.querySelector('.interactuar');
-    menu.classList.toggle('active');
-    interactuar.classList.toggle('active');
-}
